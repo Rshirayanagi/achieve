3fdedb6fff48abc100c4ebcab8273c27877f55ce
Rie Shirayanagi <shirarie9219@gmail.com>
2017-06-29 07:17:48 +0000

    mail

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612162842) do
+ActiveRecord::Schema.define(version: 20170625121403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20170612162842) do
     t.string   "name"
     t.string   "email"
     t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "syohins", force: :cascade do |t|
+    t.string   "item"
+    t.integer  "price"
+    t.text     "note"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
