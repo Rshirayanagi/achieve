f9604b9f517774dd091a95cdca6cf91de7cd41c8
Rie Shirayanagi <shirarie9219@gmail.com>
2017-07-03 05:01:36 +0000

    Ninshou

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,8 +11,10 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contacts_params)
     
+    
     if @contact.save
         redirect_to root_path, notice: "お問い合わせありがとうございました！"    
+        NoticeMailer.sendmail_contact(@contact).deliver
       else
         render 'new'
     end  
